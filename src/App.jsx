69069c42d2088fe6c701d02f3ac72630ea224afa
EjiroOsiephri <osiephriejiro765@gmail.com>
2023-04-01 17:01:19 +0100

    added context Api

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,21 @@ import Home from "./routes/Home"
 import Destination from "./routes/Destination"
 import Crew from './routes/Crew'
 import Technology from './routes/Technology'
+import AuthContext from './components/AuthContext/AuthContext'
 
 
 function App() {
     return (
-        <React.Fragment>
+        <AuthContext.Provider value={{
+            img: 'image-europa.png',
+        }}>
             <Routes>
                 <Route path='/' element={<Home />} />
                 <Route path='/destination' element={<Destination />} />
                 <Route path='/crew' element={<Crew />} />
                 <Route path='/technology' element={<Technology />} />
             </Routes>
-        </React.Fragment>
+        </AuthContext.Provider>
     )
 }
 
