6965379653fbad9cbd57756a3b13251a60e0cce5
EjiroOsiephri <osiephriejiro765@gmail.com>
2023-04-01 22:24:48 +0100

    reinitialize repo

--- src/App.jsx
+++ src/App.jsx
@@ -10,10 +10,7 @@ import AuthContext from './components/AuthContext/AuthContext'
 function App() {
     return (
         <AuthContext.Provider value={{
-            imgEuropa: 'image-europa.png',
-            imgMoon: 'image-moon.png',
-            imgMars: 'image-mars.png',
-            imgTitan: 'image-titan.png',
+            imgMoonText: 'See our planet as you’ve never seen it before. A perfect relaxing trip away to help regain perspective and come back refreshed. While you’re there, take in some history by visiting the Luna 2 and Apollo 11 landing sites.'
         }}>
             <Routes>
                 <Route path='/' element={<Home />} />
--- src/routes/Destination.jsx
+++ src/routes/Destination.jsx
@@ -1,10 +1,12 @@
 import React, { useContext } from 'react'
+import { Route, Routes } from "react-router-dom"
 import AuthContext from '../components/AuthContext/AuthContext'
 import imgEuropa from "../components/assets/destination/image-europa.png"
 import imgmoon from "../components/assets/destination/image-moon.png"
 import imgmars from "../components/assets/destination/image-mars.png"
 import imgtitan from "../components/assets/destination/image-titan.png"
 import NavBar from './NavBar'
+import Styled from "../components/sass/destination.module.scss"
 
 const Destination = () => {
     const ctx = useContext(AuthContext)
@@ -13,9 +15,12 @@ const Destination = () => {
         <div>
             <NavBar number={2} />
             <div className="container">
-                <div className="first-container-details">
+                <div className={Styled["first-container-details"]}>
                     <h1><span>01</span> Pick your Destination</h1>
                     <img src={imgmoon} alt="img-europa" />
+                </div>
+                <div className={Styled['second-container']}>
+
                 </div>
             </div>
         </div>
