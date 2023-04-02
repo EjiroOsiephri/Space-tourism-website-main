2394812eb4000bc5471ed1f181e7aa13c4d56463
EjiroOsiephri <osiephriejiro765@gmail.com>
2023-04-02 23:34:43 +0100

    Done with douglas , the commander page

--- src/routes/Crew.jsx
+++ src/routes/Crew.jsx
@@ -6,11 +6,17 @@ const Crew = () => {
     return (
         <div className={Styled['crew-home']}>
             <NavBar number={3} />
-            <div className="first-flex-container">
+            <div className={Styled["first-flex-container"]}>
                 <h1><span>02</span>MEET YOUR CREW</h1>
                 <h3>Commander</h3>
                 <h2>Douglas Hurley</h2>
                 <h5>Douglas Gerald Hurley is an American engineer, former Marine Corps pilot and former NASA astronaut. He launched into space for the third time as commander of Crew Dragon Demo-2.</h5>
+                <div className={Styled["button-navigator"]}>
+                    <button></button>
+                    <button></button>
+                    <button></button>
+                    <button></button>
+                </div>
             </div>
             <div className="second-flex-container">
 
