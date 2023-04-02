dcb62ab6ce910658907625d6b3fc9f211ec8aa55
EjiroOsiephri <osiephriejiro765@gmail.com>
2023-04-02 19:45:14 +0100

    Finished Destination page

--- src/routes/Titan.jsx
+++ src/routes/Titan.jsx
@@ -19,7 +19,7 @@ const Titan = () => {
                 <div className={Styled["first-container-details"]}>
                     <h1><span>01</span> Pick your Destination</h1>
                     <div className={Styled.img}>
-                        <img src={imgmars} alt="img-europa" />
+                        <img src={imgtitan} alt="img-europa" />
                     </div>
                 </div>
                 <div className={Styled["second-details-moon-container"]}>
@@ -30,18 +30,18 @@ const Titan = () => {
                         <Link className={Styled.border} to='/titan'>Titan</Link>
                     </div>
                     <div className={Styled.typography}>
-                        <h1>Mars</h1>
-                        <h4>Don’t forget to pack your hiking boots. You’ll need them to tackle Olympus Mons, the tallest planetary mountain in our solar system. It’s two and a half times the size of Everest!</h4>
+                        <h1>Titan</h1>
+                        <h4>The only moon known to have a dense atmosphere other than Earth, Titan is a home away from home (just a few hundred degrees colder!). As a bonus, you get striking views of the Rings of Saturn.</h4>
                     </div>
                     <hr />
                     <div className={Styled["flex-container"]}>
                         <div className={Styled["first-flex-container"]}>
                             <h5>AVG. DISTANCE</h5>
-                            <h3>225 MIL. km</h3>
+                            <h3>1.6 BIL. km</h3>
                         </div>
                         <div className={Styled["second-flex-container"]}>
                             <h5>EST. TRAVEL TIME</h5>
-                            <h3>9 months</h3>
+                            <h3>7 years</h3>
                         </div>
                     </div>
                 </div>
